bc405d83f7a31ee5e177b930ba660f90079209ef
NathanLee2021 <818998@mystma.org>
2021-02-23 17:00:11 +0000

    remove evens

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#WRITE YOUR CODE IN THIS FILE
+def removeEvens(k):
+    newList = k
+    numPopped = 0
+    for i in range (0, len(k)):
+        if newList[i - numPopped] % 2 == 0 and newList[i - numPopped] != 0:
+            newList.pop(i - numPopped)
+            numPopped = numPopped + 1
+    return newList
